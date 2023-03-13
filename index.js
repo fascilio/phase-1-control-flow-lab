1362f2b73f5712cb484965805a5da9f66e12578c
Samuel Karanja <samuel.karanja@student.moringaschool.com>
2023-03-13 19:21:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(Value){
   // Write your code here!
+  if(Value <= 400){
+   return "This one is on me!";
+  }else if(Value > 2000 && Value <= 2500){
+    return "I will gladly take your thirty bucks."
+  }else{
+    return 'No can do.'
+  }
+  }
+   
+
+
+function ternaryCheckCity(City){
+  // Write your code here!
+  return City === 'NYC'?'Ok, sounds good.':'No go.';
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(Tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(Tip){
+    case 'generous':
+    return  'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return 'Bye.'
+  }
 }
